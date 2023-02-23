2e71480a22712e802dd23e3ef521cf9793467aaa
GH-2018-prod <maancuza@gmail.com>
2023-02-23 11:51:36 -0600

    Deploy root files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mntcrv3/',
+  base: 'https://gh-2018-prod.github.io/mntcrv3/',
   plugins: [react()],
 })
