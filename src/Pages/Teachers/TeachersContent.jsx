a0c400729ca5801479cbe1f66ea22659beef6447
GH-2018-prod <maancuza@gmail.com>
2023-10-25 20:06:13 -0600

    adding two more rows

--- src/Pages/Teachers/TeachersContent.jsx
+++ src/Pages/Teachers/TeachersContent.jsx
@@ -28,9 +28,51 @@ export const TeachersContent = () => {
         </Col>
       </Row>
 
+      <Row className='pt-3 pb-4'>
+        <Col lg={8} md={6}>
+          <h4 className='pb-3 text-center'>Founder 1</h4>
+          <p>
+            Como propietaria de este campus, la prioridad desde sus inicios fue la enseninza conciente a partir de los maestros y hacia los estudiantes. Por esta razon, la comprension activa y conciente de todo el equipo de native teacher ha procuparado dar todo el conocimiento que poseemos.
+          </p>
+          <p>
+            ponemos a la orden todos nuestros medios para que nuestros estudiantes alcancen todas sus metas
+          </p>
+          <p>
+            Mi nombre es Teacher Jennifer y les deseo lo mejor en este viaje!s
+          </p>
+        </Col>
+        <Col lg={4} md={6}>
+          <img src="loginimage.jpeg" alt="" width="200px"/>
+          <p>
+            <em>Teacher Jennifer</em>        
+          </p>
+        </Col>
+      </Row>
+
+      <Row className='pt-3 pb-4'>
+        <Col lg={8} md={6}>
+          <h4 className='pb-3 text-center'>Founder 2</h4>
+          <p>
+            Como propietaria de este campus, la prioridad desde sus inicios fue la enseninza conciente a partir de los maestros y hacia los estudiantes. Por esta razon, la comprension activa y conciente de todo el equipo de native teacher ha procuparado dar todo el conocimiento que poseemos.
+          </p>
+          <p>
+            ponemos a la orden todos nuestros medios para que nuestros estudiantes alcancen todas sus metas
+          </p>
+          <p>
+            Mi nombre es Teacher Jennifer y les deseo lo mejor en este viaje!s
+          </p>
+        </Col>
+        <Col lg={4} md={6}>
+          <img src="loginimage.jpeg" alt="" width="200px"/>
+          <p>
+            <em>Teacher Jennifer</em>        
+          </p>
+        </Col>
+      </Row>
+
       <Row  className='pt-3 pb-4'>
         <Col lg={8} md={6}>
-          <h4 className='pb-3 text-center'>Teacher</h4>
+          <h4 className='pb-3 text-center'>Teacher 3</h4>
           <p>
             Soy una persona aventurera. Como un buen boy scout siempre estoy listo. Hay retos en la vida que me dan miedo como fue el de empezar a enseniar. Me aterraba el hecho de hablar en publico pero gracias a que decide tomar el primer paso, ahora soy un feliz Teacher de My Native Teacher.
           </p>
